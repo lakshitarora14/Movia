791c73173b2857b37debf4a6da12866336bb114f
Lakshit Arora <lakshitarora14@gmail.com>
2022-03-20 16:24:00 +0530

    added image slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { ChevronDoubleUpIcon } from '@heroicons/react/solid'
 import { Line } from 'rc-progress'
 import { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Slider from 'react-slick/lib/slider'
 import MovieCard from '../components/MovieCard'
 import { getTrendingMovies } from '../context/MovieActions'
 import MovieContext from '../context/MovieContext'
 function Home() {
+  const navigate = useNavigate()
   const { dispatch, trending, page } = useContext(MovieContext)
   const [visible, setVisible] = useState(false)
   const [loading] = useState()
+  const [imageSlider, setImageSlider] = useState([])
   const [hasMore, setHasMore] = useState(true)
   const observer = useRef()
   const lastMovie = useCallback(
@@ -36,6 +40,18 @@ function Home() {
     })
     const getData = async () => {
       const data = await getTrendingMovies(page)
+      if (page === 1) {
+        const result = []
+        data.results.slice(0, 5).forEach((i) =>
+          result.push({
+            title: i.title,
+            backdropPath: i.backdrop_path,
+            desc: i.overview,
+            id: i.id
+          })
+        )
+        setImageSlider(result)
+      }
       page === data.total_pages && setHasMore(false)
       dispatch({ type: 'SET_TRENDING', payload: data.results })
     }
@@ -63,11 +79,55 @@ function Home() {
   if (loading) {
     return <div className='h12'>Loading</div>
   }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+  }
+
   return (
     <div className='pt-28'>
       <div className='hidden md:flex text-6xl p-10 font-semibold tracking-wider	'>
         Trending
       </div>
+      <Slider {...settings} className='mt-40 md:mt-0 object-fill mb-4'>
+        {imageSlider?.map((i) => {
+          return (
+            <div
+              key={i.title}
+              className='relative max-h-[20%] md:h-[500px] 3xl:h-[700px]'
+            >
+              <img
+                className='float-right'
+                style={{ width: '100%', objectFit: 'contain' }}
+                loading='lazy'
+                src={`https://image.tmdb.org/t/p/w1280/${i.backdropPath}`}
+                alt={i.title}
+              />
+              <div className='absolute bg-gradient-to-r from-black h-[510px] w-full top-0'></div>
+              <div className='absolute m-2 z-50 bottom-4 md:bottom-8 ml-6 md:ml-12 max-w-[50%] max-h-[80%] overflow-clip'>
+                <div className='text-3xl md:text-6xl overflow-hidden font-semibold'>
+                  {i.title}
+                </div>
+                <div className='hidden md:flex text-2xl w-[60%] my-4 font-light h-[230px] overflow-hidden'>
+                  {i.desc}
+                </div>
+                <button
+                  className='h-12 bg-black/70 md:bg-[#A741EB]/70 p-1 py-2 md:p-2 font-light text-lg md:text-3xl mt-4'
+                  onClick={() => navigate(`/movie/${i.id}`)}
+                >
+                  Info
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </Slider>
       <button
         style={{ display: visible ? 'block' : 'none' }}
         className='fixed bottom-10 right-10 z-10'
@@ -76,7 +136,7 @@ function Home() {
         <ChevronDoubleUpIcon className='bg-[#A741EB] h-6 md:h-10 xl:h-14 rounded-full p-2'></ChevronDoubleUpIcon>
       </button>
       <div
-        className='px-8 mt-28 md:mt-0 sm: grid
+        className='px-8 md:mt-0 sm: grid
       sm:grid
       md:grid-cols-2
       lg:grid-cols-3
